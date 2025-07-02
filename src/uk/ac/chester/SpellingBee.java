6d14fb3806967a3d878f8fd7ea9cafc14a977535
Josh Birch <joshuabirch@protonmail.com>
2025-07-02 15:18:23 +0100

    Finish problem 2 spelling bee

--- src/uk/ac/chester/SpellingBee.java
+++ src/uk/ac/chester/SpellingBee.java
@@ -1,8 +1,10 @@
 package uk.ac.chester;
 
-import java.lang.reflect.Array;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class SpellingBee
@@ -18,7 +20,7 @@ public class SpellingBee
         System.out.println("SPELLING BEE\n" +
                 "Create as many valid 4+ letter words using the given letters as you can.\n" +
                 "Words must ALWAYS contain the middle letter.\n" +
-                "To exit the game, type 'EXIT' in all caps\n\n");
+                "To exit the game, type 'EXIT1' in all caps\n\n");
 
         // middle letter
         while (true)
@@ -26,7 +28,7 @@ public class SpellingBee
             System.out.println("Enter the middle letter:");
             String middleLetter = inputScanner.nextLine();
 
-            if (middleLetter.equals("EXIT"))
+            if (middleLetter.equals("EXIT1"))
             {
                 System.out.println("Thanks for playing!");
                 System.exit(0);
@@ -46,7 +48,7 @@ public class SpellingBee
             System.out.println("\nEnter the other letters:");
             String otherLetters = inputScanner.nextLine();
 
-            if (otherLetters.equals("EXIT"))
+            if (otherLetters.equals("EXIT1"))
             {
                 System.out.println("Thanks for playing!");
                 System.exit(0);
@@ -60,19 +62,28 @@ public class SpellingBee
         {
             System.out.println("\nPlease enter a guess: ");
             String currentGuess = inputScanner.nextLine();
+            currentGuess = currentGuess.toUpperCase();
 
-            if (currentGuess.equals("EXIT"))
+            if (currentGuess.equals("EXIT1"))
             {
                 System.out.println("Thanks for playing!");
                 System.exit(0);
             }
 
-            if (guessIsLongEnough(currentGuess)
-                    && containsMiddleLetter(currentGuess, allowedLetters.getFirst())
-                    && containsOnlyAllowedLetters(currentGuess, allowedLetters))
+            if (currentGuess.equals("CH34TC0DE"))
+            {
+                System.out.println("\nCHEAT ACTIVATED! Here are all the possible words from your chosen letters: ");
+                cheatCode(allowedLetters);
+            }
+
+            else if (guessIsLongEnough(currentGuess)
+                    && containsMiddleLetter(currentGuess, allowedLetters.get(0))
+                    && containsOnlyAllowedLetters(currentGuess, allowedLetters)
+                    && isDictionaryWord(currentGuess)
+                    && notAlreadyGuessed(currentGuess, correctGuesses))
             {
                 correctGuesses.add(currentGuess);
-                System.out.println("Nice! " + currentGuess + " is valid. You now have " +
+                System.out.println("\nNice! " + currentGuess + " is valid. You now have " +
                         correctGuesses.size() + " correct word(s).");
             }
         }
@@ -83,13 +94,13 @@ public class SpellingBee
     {
         if (letter.isEmpty())
         {
-            System.out.println("Middle letter cannot be empty!");
+            System.out.println("Middle letter cannot be empty!\n");
             return false;
         }
 
         if (letter.length() > 1)
         {
-            System.out.println("Middle letter must only be one character!");
+            System.out.println("Middle letter must only be one character!\n");
             return false;
         }
 
@@ -97,7 +108,7 @@ public class SpellingBee
 
         if (!Character.isLetter(c))
         {
-            System.out.println("Middle letter must be a letter! (Not a number or symbol...)");
+            System.out.println("Middle letter must be a letter! (Not a number or symbol...)\n");
             return false;
         }
 
@@ -124,7 +135,7 @@ public class SpellingBee
         {
             if (!Character.isLetter(c))
             {
-                System.out.println("Only valid lettters allowed!");
+                System.out.println("Only valid letters allowed!");
                 return;
             }
         }
@@ -162,22 +173,108 @@ public class SpellingBee
 
     private static boolean containsOnlyAllowedLetters(String word, ArrayList<Character> allowedLetters)
     {
-        ;
         char[] wordAsArray = word.toCharArray();
 
         for (char c : wordAsArray)
         {
             if (!allowedLetters.contains(c))
             {
-                System.out.println("Word must only contain the allowed letters! Allowed letters are: ");
+                System.out.println("Letter " + c + "is not allowed! Allowed letters are: ");
+
                 for (Character allowedLetter : allowedLetters)
                 {
                     System.out.print(allowedLetter + " ");
                 }
+                System.out.println();
                 return false;
             }
         }
         return true;
     }
 
+    private static boolean isDictionaryWord(String word)
+    {
+        ArrayList<String> dictionaryWords = new ArrayList<>();
+        File dictionaryFile = new File("resources/wordlist.txt");
+
+        try
+        {
+            Scanner fileScanner = new Scanner(dictionaryFile);
+
+            while (fileScanner.hasNext())
+            {
+                String validWord = fileScanner.nextLine();
+                dictionaryWords.add(validWord);
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("(Dictionary file could not be read)");
+            System.out.println(e.getMessage());
+        }
+
+        if (dictionaryWords.contains(word.toLowerCase()))
+        {
+            return true;
+        }
+        else
+        {
+            System.out.println(word + " is not a valid dictionary word.");
+            return false;
+        }
+    }
+
+
+    private static boolean notAlreadyGuessed(String word, ArrayList<String> guesses)
+    {
+        if (guesses.contains(word))
+        {
+            System.out.println(word + " has already been guessed!");
+            return false;
+        }
+
+        return true;
+    }
+
+
+    private static void cheatCode(ArrayList<Character> validLetters)
+    {
+        ArrayList<String> cheatWords = new ArrayList<>();
+        File dictionaryFile = new File("resources/wordlist.txt");
+        PrintStream unsilenced = System.out;
+        PrintStream silenced = new PrintStream(OutputStream.nullOutputStream());
+
+        try
+        {
+            Scanner fileScanner = new Scanner(dictionaryFile);
+            System.setOut(silenced);
+
+            while (fileScanner.hasNextLine())
+            {
+                String dictionaryWord = fileScanner.nextLine().toUpperCase();
+
+                if (guessIsLongEnough(dictionaryWord)
+                    && containsMiddleLetter(dictionaryWord, validLetters.get(0))
+                    && containsOnlyAllowedLetters(dictionaryWord, validLetters))
+                {
+                    cheatWords.add(dictionaryWord);
+                }
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.setOut(unsilenced);
+            System.out.println("(Dictionary file could not be read)");
+            System.out.println(e.getMessage());
+        }
+        finally
+        {
+            System.setOut(unsilenced);
+        }
+
+        for (String word : cheatWords)
+        {
+            System.out.println(word);
+        }
+    }
 }
